5ce535b1c88d2b97d4aa7076aeebb5b306860642
Karen Lu <karenlu1209@karen-lus-macbook-pro.local>
2016-04-10 21:02:16 +0800

    Initial Commit

---
+++ BookClub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BookClub
+//
+//  Created by Karen Lu on 4/10/16.
+//  Copyright © 2016 Karen Lu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
